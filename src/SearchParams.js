63733cd626768f7cf13d84127c1e071ef2b01bbb
Jock Dalby <jock.dalby@citrusad.com>
2022-11-22 09:00:59 +1000

    use tailwind forms plugin to style the search form

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -29,7 +29,7 @@ const SearchParams = () => {
   return (
     <div className="my-0 mx-auto w-11/12">
       <form
-      className="p-10 mb-10 rounded-lg bg-gray-200 shadow-lg flex flex-col justify-center items-center"
+        className="p-10 mb-10 rounded-lg bg-gray-200 shadow-lg flex flex-col justify-center items-center"
         onSubmit={(e) => {
           e.preventDefault();
           requestPets();
@@ -39,8 +39,10 @@ const SearchParams = () => {
           Location
           <input
             id="location"
+            type="text"
             value={location}
             placeholder="Location"
+            className="w-60 mb-5 block"
             onChange={(e) => updateLocation(e.target.value)}
           />
         </label>
@@ -49,6 +51,7 @@ const SearchParams = () => {
           <select
             id="animal"
             value={animal}
+            className="w-60 mb-5 block"
             onChange={(e) => {
               updateAnimal(e.target.value);
               updateBreed("");
@@ -72,6 +75,7 @@ const SearchParams = () => {
             disabled={!breeds.length}
             id="breed"
             value={breed}
+            className="w-60 mb-5 block disabled:opacity-50"
             onChange={(e) => updateBreed(e.target.value)}
             onBlur={(e) => updateBreed(e.target.value)}
           >
@@ -87,6 +91,7 @@ const SearchParams = () => {
           Theme
           <select
             value={theme}
+            className="w-60 mb-5 block"
             onChange={(e) => setTheme(e.target.value)}
             onBlur={(e) => setTheme(e.target.value)}
           >
@@ -96,7 +101,12 @@ const SearchParams = () => {
             <option value="mediumorchid">Medium Orchid</option>
           </select>
         </label>
-        <button style={{ backgroundColor: theme }}>Submit</button>
+        <button
+          className="rounded px-6 py-2 color text-white hover:opacity-50 border-none"
+          style={{ backgroundColor: theme }}
+        >
+          Submit
+        </button>
       </form>
       <Results pets={pets} />
     </div>
